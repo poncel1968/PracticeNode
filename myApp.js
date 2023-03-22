6cd0234b2d5d15f0ea3654eb71e96eab8716d491
Replit user <>
2023-03-22 15:35:18 -0300

    git commit -a

--- myApp.js
+++ myApp.js
@@ -1,7 +1,27 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config();
 
+console.log("Hello World");
+
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+console.log(process.env.MESSAGE_STYLE);
+
+app.get("/json",function(req,res){
+  if(process.env.MESSAGE_STYLE==="uppercase"){
+    res.json( {"message": "HELLO JSON"} );
+    console.log("Entró en UPPERCASE");
+  } else{
+    res.json( {"message": "Hello json"} );
+    console.log("Entro en uppercase");
+  }
+});
 
 
 
